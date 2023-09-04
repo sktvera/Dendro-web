dc5613a10f5f5ade2f248c4f1dac1421bdaacbf8
sktvera <veeraa1270@gmail.com>
2023-09-03 19:49:47 -0500

    Dendro app

---
+++ src/pages/Shop.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+import CommonSection from "../components/UI/CommonSection";
+import Helmet from "../components/Helmet/Helmet";
+import { Container, Row, Col } from "reactstrap";
+
+import "../styles/shop.css";
+
+import ProductsList from "../components/UI/ProductsList";
+import useGetData from "../custom-hooks/useGetData";
+
+const Shop = () => {
+  const { data: products} = useGetData("products");
+ 
+
+  return (
+    <Helmet title="Shop">
+      <CommonSection title="Products" />
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="3" md="6">
+              <div className="filter__widget">
+                <select>
+                  <option>Filter By Category</option>
+                  <option value="sofa">Sofa</option>
+                  <option value="mobile">Mobile</option>
+                  <option value="chair">Chair</option>
+                  <option value="watch">Watch</option>
+                  <option value="wireless">Wireless</option>
+                </select>
+              </div>
+            </Col>
+            <Col lg="3" md="6" className="text-end">
+              <div className="filter__widget">
+                <select>
+                  <option>Sort By</option>
+                  <option value="ascending">Ascending</option>
+                  <option value="descending">Descending</option>
+                </select>
+              </div>
+            </Col>
+            <Col lg="6" md="12">
+              <div className="search__box">
+                <input
+                  type="text"
+                  placeholder="Search......"
+                 
+                />
+                <span>
+                  <i class="ri-search-line"></i>
+                </span>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section className="pt-0">
+        <Container>
+          <Row>
+            {products.length === 0 ? (
+              <h1 className="text-center fs-4">No products are found!</h1>
+            ) : (
+              <ProductsList data={products} />
+            )}
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default Shop;
